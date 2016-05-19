ab72b351dd62f5fae1e5effdc0b7d350538cc267
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-05-19 17:14:27 +0700

    libra: Add Xiaomi Mi-4s support
    Change-Id: If279288617f592f9277692f3285d68e687a8543e

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter libra, $(TARGET_DEVICE)),)
+ifneq ($(filter libra aqua, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
