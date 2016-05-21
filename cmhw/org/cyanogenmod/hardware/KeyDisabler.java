daf675b7545d43d5e05dfc55e19e386f6d21ff91
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-05-22 01:28:32 +0700

    libra: More aqua adaptation
    No on-screen navigation bar for aqua for now
    
    Change-Id: I39dbdccb09c40998f3dd3b5f6ce56a090343ab3a

--- cmhw/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/org/cyanogenmod/hardware/KeyDisabler.java
@@ -17,6 +17,7 @@
 package org.cyanogenmod.hardware;
 
 import org.cyanogenmod.hardware.util.FileUtils;
+import java.io.File;
 
 /*
  * Disable capacitive keys
@@ -31,7 +32,9 @@ public class KeyDisabler {
 
     private static String CONTROL_PATH = "/proc/synaptics_dsx/0dbutton";
 
-    public static boolean isSupported() { return true; }
+    public static boolean isSupported() {
+        return new File(CONTROL_PATH).exists();
+    }
 
     public static boolean isActive() {
         return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
