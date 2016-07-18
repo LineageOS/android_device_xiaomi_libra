37debf9548691ddc1d074aab23586edce6370293
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-07-18 14:19:52 +0700

    libra: Force build libbt-vendor 64bit version
    * ANT bluetooth transport depend on 64bit version
      of libbt-vendor but bluetooth framework build 32bit
      version only
    * Explicit add libbt-vendor package to force build
      64bit version.
    
    Change-Id: Ib015614ebbf5e570a72dc52d6f241daa8cc9e0f4

--- device.mk
+++ device.mk
@@ -252,7 +252,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library \
-    libantradio
+    libantradio \
+    libbt-vendor
 
 DEVICE_PACKAGE_OVERLAYS := \
     device/xiaomi/libra/overlay
