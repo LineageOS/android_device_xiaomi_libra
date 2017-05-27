296c072eabc052a8edffef289b34944886cb6891
Demon000 <demonsingur@gmail.com>
2017-05-27 15:21:21 +0300

    configs: move sensor configs to separate directories
    Change-Id: I3cae6dffc22c588ec1e9ff3db036e461bf02ac26

--- product/qcom-sensors.mk
+++ product/qcom-sensors.mk
@@ -1,5 +1,6 @@
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hals.conf:system/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
 PRODUCT_PACKAGES += \
     sensors.msm8992
