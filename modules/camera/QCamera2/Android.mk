90a22bedce60d2ea97e74f0200b525c28af17754
Demon000 <demonsingur@gmail.com>
2017-06-20 22:55:01 +0300

    camera: fix include paths
    Change-Id: I99fa9e6f7fa30c30eb6b2f76e70af34f92d0ac86

--- modules/camera/QCamera2/Android.mk
+++ modules/camera/QCamera2/Android.mk
@@ -60,7 +60,7 @@ LOCAL_C_INCLUDES := \
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
         frameworks/native/include/media/hardware \
-        hardware/qcom/camera/QCamera2/HAL
+        device/xiaomi/libra/modules/camera/QCamera2/HAL
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
