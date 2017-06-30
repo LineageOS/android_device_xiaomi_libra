505ec9be57ed53c1626b2af33705030ccfa59a2d
Demon000 <demonsingur@gmail.com>
2017-06-30 12:24:23 +0300

    post_boot: enable retention idle states
    We've fixed this in kernel.
    
    Change-Id: I18269fe86d9b1d2018d750777282311c2f14aed2

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -54,18 +54,6 @@ do
     echo -n enable > $mode
 done
 
-# Disable CPU retention
-echo 0 > /sys/module/lpm_levels/system/a53/cpu0/retention/idle_enabled
-echo 0 > /sys/module/lpm_levels/system/a53/cpu1/retention/idle_enabled
-echo 0 > /sys/module/lpm_levels/system/a53/cpu2/retention/idle_enabled
-echo 0 > /sys/module/lpm_levels/system/a53/cpu3/retention/idle_enabled
-echo 0 > /sys/module/lpm_levels/system/a57/cpu4/retention/idle_enabled
-echo 0 > /sys/module/lpm_levels/system/a57/cpu5/retention/idle_enabled
-
-# Disable L2 retention
-echo 0 > /sys/module/lpm_levels/system/a53/a53-l2-retention/idle_enabled
-echo 0 > /sys/module/lpm_levels/system/a57/a57-l2-retention/idle_enabled
-
 # Configure governor settings for little cluster
 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1440000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
