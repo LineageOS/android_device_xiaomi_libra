c62d3cfea427b2dbfa9940db3a655d5f2362d409
Demon000 <demonsingur@gmail.com>
2017-08-29 17:28:51 +0300

    display: remove extra packages
    Change-Id: I64e035ce894c548e9301e40172df077307b35f5c

--- device.mk
+++ device.mk
@@ -124,14 +124,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8992 \
     gralloc.msm8992 \
     hwcomposer.msm8992 \
     memtrack.msm8992 \
     libgenlock \
-    liboverlay \
-    libqdutils \
-    libqdMetaData \
     libtinyxml
 
 PRODUCT_PACKAGES += \
