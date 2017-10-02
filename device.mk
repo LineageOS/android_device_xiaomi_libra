ce42dda6c2f3e6192f5fdb73cf9a088cead7ab70
Demon000 <demonsingur@gmail.com>
2017-10-02 21:04:34 +0300

    vibrator: replace cmhw intensity with lineage interface
    Change-Id: I05a535d143ea9a91b6302a56212b3f92029132d6

--- device.mk
+++ device.mk
@@ -299,7 +299,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl.lineage
 
 # WiFi
 PRODUCT_PACKAGES += \
